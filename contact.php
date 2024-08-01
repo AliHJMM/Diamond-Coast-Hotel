facdd09577fcdb8d5ca4c54c87f6f7c818ee1fb9
Ali <AliHasanJMM@gmail.com>
2024-08-01 19:43:24 +0300

    included specialRequest.php

--- contact.php
+++ contact.php
@@ -200,15 +200,7 @@
           </div>
         </div>
 
-        <div class="untree_co--site-section py-5 bg-body-darker cta">
-          <div class="container">
-            <div class="row">
-              <div class="col-12 text-center">
-                <h3 class="m-0 p-0">If you have any special requests, please feel free to call us. <a href="tel://+123456789012">+12.345.678.9012</a></h3>
-              </div>
-            </div>
-          </div>
-        </div>
+        <?php include 'specialRequest.php'; ?>
 
       </main>
       <?php include 'footer.php'; ?>
