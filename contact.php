01bd54379e6f4376c30fac9b87357ecafb68f5e1
Ali <AliHasanJMM@gmail.com>
2024-08-01 21:36:55 +0300

    Favicons

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" href="favicon.png">
+  <link rel="shortcut icon" href="DiamondCoastHotelLogo.png">
 
   <meta name="description" content="" />
   <meta name="keywords" content="" />
--- home.php
+++ home.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['username'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="shortcut icon" href="favicon.png">
+  <link rel="shortcut icon" href="DiamondCoastHotelLogo.png">
 
   <meta name="description" content="" />
   <meta name="keywords" content="" />
