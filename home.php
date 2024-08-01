616da3fc5cc8bd0dfffbad1b6247119d0d84cebd
Ali <AliHasanJMM@gmail.com>
2024-08-01 22:36:17 +0300

    remove news section

--- home.php
+++ home.php
@@ -308,48 +308,6 @@ if (!isset($_SESSION['username'])) {
           </div>
         </div>
         
-
-        
-        
-
-        <div class="untree_co--site-section">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-4 section-heading" data-aos="fade-up">
-                <h3 class="text-left">News &amp; Events</h3>
-                <div class="w-75">
-                  <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                  </div>
-                <p><a href="#" class="readmore">All Posts</a></p>
-              </div>
-              <div class="col-md-4">
-                <div class="post-entry" data-aos="fade-up" data-aos-delay="100">
-                  <a href="#" class="thumb"><img src="images/img_1.jpg" alt="Image" class="img-fluid"></a>
-                  <div class="post-entry-contents">
-                    <h3><a href="#">How algorithms shape our world</a></h3>
-                    <div class="date">December 20, 2019 &bullet; by <a href="#">Joefrey Mahusay</a></div>
-                    <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                    <p><a href="#" class="readmore">Read more</a></p>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-4">
-                <div class="post-entry" data-aos="fade-up" data-aos-delay="200">
-                  <a href="#" class="thumb"><img src="images/img_3.jpg" alt="Image" class="img-fluid"></a>
-                  <div class="post-entry-contents">
-                    <h3><a href="#">How algorithms shape our world</a></h3>
-                    <div class="date">December 20, 2019 &bullet; by <a href="#">Joefrey Mahusay</a></div>
-                    <p>A hotel is an establishment that provides paid lodging on a short-term basis.</p>
-                    <p><a href="#" class="readmore">Read more</a></p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-    
         <?php include 'specialRequest.php'; ?>
 
 
